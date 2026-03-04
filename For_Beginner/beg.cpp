57b57bb13f8134f7672505a81dd0873ea3deb729
Prabhat Amgain <amgainparbhat@gmail.com>
2026-03-04 20:15:22 +0545

    Add files via upload

---
+++ For_Beginner/beg.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>  
+//This line includes almost all the standard C++ libraries at once.
+using namespace std;  
+void solve(){
+// This is where the main logic of your program goes for each test case.
+}
+ int main() {
+ 	
+    ios::sync_with_stdio(false);  
+    // This disables the synchronization between C++ and C standard streams.
+    // Makes input/output faster in competitive programming.
+    
+    cin.tie(0); cin.tie(0); 
+    // This unties cin from cout.
+    // Normally, cin waits for cout to flush output before reading input.
+    // Untying makes input faster.
+    
+    int t;
+    cin >> t;  
+    // Reads the number of test cases from the user.
+
+     while(t--){ // This loop runs exactly 't' times.
+     solve();  // Calls the 'solve' function for each test case.
+    }
+  return 0;  
+}
